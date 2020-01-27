d29299295587339b6c3bf7f513cd2f3a2f552e34
Gabriel Rodrigues Souza <gabriel.souza@cotriguacu.com.br>
2020-01-27 10:51:40 -0300

    alter

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 //usa a bliblioteca axios para executar as requisições na api
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:3001/api" });
+const api = axios.create({ baseURL: "http://100.117.34.234:3001/api" });
 
 export default api;
